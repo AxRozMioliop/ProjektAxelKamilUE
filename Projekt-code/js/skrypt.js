d108f35eef441db8cf38a61300d6d340585d4e29
Mioliop <45671682+AxRozMioliop@users.noreply.github.com>
2022-01-09 16:54:15 +0100

    Add global variables and new functions

---
+++ Projekt-code/js/skrypt.js
@@ -0,0 +1,14 @@
+let zalogowany = false;
+let imieUzytk = '';
+let nazwiskoUzytk = '';
+let emailUzytk = '';
+let rezerwacjeUzytk = [];
+let dataRejestracji = "";
+
+function zaloguj() {
+  zalogowany = true;
+}
+
+function rejestruj(){
+
+}
